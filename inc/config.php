50abe3a2a607273465244e32e2d01fc506386721
dmarkKd <kuldeeps@dmarkweb.com>
2017-09-12 12:59:56 -0700

    new

--- inc/config.php
+++ inc/config.php
@@ -2,10 +2,7 @@
 define('APP_URL','https://apps.shopify.com/dm-product-customizer');
 //define('API_KEY','56c8bd41de37028cc8ac422fc3c23da8');
 //define('APP_SECRET','c912e62c1e95c034c9832157d249a8d3');
-define('APP_BASE_URL', 'http://dm-custmizer.herokuapp.com');
-
+define('APP_BASE_URL', 'http://dm-custmizer.herokuapp.com/web');
 define('API_KEY','da3ad482e3b89141d7ca3dbe1c169092');
 define('APP_SECRET','5bd15fc40f745591df6303b4f0fb08e2');
-
-
-define('STORES_DB', 'stores.json');		
+define('STORES_DB', 'stores.json');
